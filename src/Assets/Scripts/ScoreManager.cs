a7032853ad735b0a0c7c73d0abae0a7412042984
Ryan-Blignaut-BBD <ryan.blignaut@bbd.co.za>
2024-08-11 19:20:27 +0200

    background stuff music stuff maybe better text?

---
+++ src/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,20 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    private TextMeshProUGUI score;
+    private int currentScore;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
--- src/Assets/Scripts/Turtle/TurtleScript.cs
+++ src/Assets/Scripts/Turtle/TurtleScript.cs
@@ -1,40 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class TurtleScript : MonoBehaviour
+namespace Turtle
 {
-    public float moveSpeed = 5f;
-    public LogicManagerScript logic;
-    
-    // screen boundaries
-    private const float MinX = -3.8f;
-    private const float MaxX = 3.8f;
-    private const float MinY = -2f;
-    private const float MaxY = 2f;
-    
-    void Start()
+    public class TurtleScript : MonoBehaviour
     {
-        logic = GameObject
-            .FindWithTag("Logic")
-            .GetComponent<LogicManagerScript>();
-    }
+        public float moveSpeed = 5f;
+        public LogicManagerScript logic;
+        private Camera _camera;
 
-    void Update()
-    {
-        MoveWithMouse();
-    }
-    
-    void MoveWithMouse()
-    {
-        // Exspensive, there's probably a "cheaper" way to do this
-        var camera = Camera.main;
-        if (!camera)
-            return;
-        var mousePosition = camera.ScreenToWorldPoint(Input.mousePosition);
-        var clampedX = Mathf.Clamp(mousePosition.x, MinX, MaxX);
-        var clampedY = Mathf.Clamp(mousePosition.y, MinY, MaxY);
-        var targetPosition = new Vector3(clampedX, clampedY, transform.position.z);
-        transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+        // screen boundaries
+        public float MinX, MinY, MaxX, MaxY;
+
+
+        private void Start()
+        {
+            _camera = Camera.main;
+            logic = GameObject
+                .FindWithTag("Logic")
+                .GetComponent<LogicManagerScript>();
+        }
+
+        private void Update()
+        {
+            MoveWithMouse();
+        }
+
+        private void MoveWithMouse()
+        {
+            if (!_camera) return;
+            var mousePosition = _camera.ScreenToWorldPoint(Input.mousePosition);
+            var clampedX = Mathf.Clamp(mousePosition.x, MinX, MaxX);
+            var clampedY = Mathf.Clamp(mousePosition.y, MinY, MaxY);
+            var targetPosition = new Vector3(clampedX, clampedY, transform.position.z);
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+        }
     }
 }
